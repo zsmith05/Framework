1331547c2ed045bbca0835351f9861f2fb71b53d
zsmith05 <102535745+zsmith05@users.noreply.github.com>
2022-09-27 13:02:45 -0500

    gredg

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ var btn = document.getElementsByClassName("modbtn");
 
 var close = document.getElementsByClassName("close");
 
-btn.onclick = function() {
-    modal.style.display = "block";
+function drop(){
+    document.getElementsByClassName("modal").style.display="block";
 }
-
 
 close.onclick = function() {
     modal.style.display = "none";
