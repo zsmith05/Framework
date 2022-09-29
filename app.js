cea04b10bf24201200293f915e4219710bab4cda
zsmith05 <102535745+zsmith05@users.noreply.github.com>
2022-09-29 11:26:34 -0500

    files

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-var modal = document.getElementById("modal");
+var modal = document.getElementsByClassName("modal");
 
-var btn = document.getElementById("modbtn");
+var btn = document.getElementsByClassName("modbtn");
 
-var closed = document.getElementById("close");
+var close = document.getElementsByClassName("close");
 
 function drop(){
-    modal.style.display = "block";
-    btn.style.display = "none"
+    document.getElementsByClassName("modal").style.display="show";
 }
 
-function closeModal() {
+close.onclick = function() {
     modal.style.display = "none";
-    btn.style.display = "block";
 }
